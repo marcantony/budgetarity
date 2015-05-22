54363d255bc017476e9f87109dc0e5b29450b75c
mpetracca26 <mrp26@vt.edu>
2015-05-21 21:18:57 -0400

    Figuring out Parse for Charities

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,11 @@ angular.module('app', ['ngRoute', 'main', 'charity'])
     .config(function ($locationProvider) {
         $locationProvider.html5Mode(true);
     })
+    .run(function () {
+    	Parse.initialize("7as8XJVDNefTa0cXQTMR5nuS6dhwQaKMHnhFgpqr", "IivItsZKeVAEXLdIUt0lpeKyPgHnScseejaAH7Hu");
+    	var Charities = Parse.Object.extend("CharityDetails");
+        var charities = new Charities();
+    })
     .controller('MainCtrl', function () {
 
     })
@@ -28,7 +33,7 @@ angular.module('app', ['ngRoute', 'main', 'charity'])
                         return budget - (periodInitialBalance - res.data.balance);
                     });
             },
-            getCharities: function () {
+            updateCharities: function () {
                 return [
                     {
                         name: 'Red Cross',
@@ -57,8 +62,21 @@ angular.module('app', ['ngRoute', 'main', 'charity'])
                     }
                 ];
             },
-            updateCharities: function (charities) {
-                // send charities to back-end
+            getCharities: function (charities) {
+                var query = new Parse.Query(charities);
+                query.find({
+  					success: function(charities) {
+  						var CharityName = charities.get('Name');
+  						var CharityAmount = charities.get('Amount');
+  						console.log("results: " + results);
+  				},
+  					error: function(error) {
+  						console.log("Error: " + error);
+  				}
+
+  				
+  				
+			});
             }
         };
     })
